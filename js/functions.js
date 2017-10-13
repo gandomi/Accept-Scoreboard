cfc5ab487695f713414a42c6d9779f3887ec81ca
Ali Gandomi <gandomi313@gmail.com>
2017-10-13 17:03:00 +0330

    Add Accept 3 scoreboard

--- js/functions.js
+++ js/functions.js
@@ -107,7 +107,7 @@ function create_stylish_table(table_selector){
 
     // designer
     time = new Date();
-    $(table_selector + " + p").html("<br>Created by: Ali Gandomi<br>Last Update "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()).addClass("designer");
+    $(table_selector + " + p").html(/*"<br>Created by: Ali Gandomi<br>"+*/"Last Update "+time.getHours()+":"+time.getMinutes()+":"+time.getSeconds()).addClass("designer");
 }
 
 function compare(){
@@ -139,117 +139,131 @@ function slider(){
         var row = pending.parents("tr:first");
         var other_row = row.siblings('tr');
         pending.css('cursor', 'default');
-        // pending.animate({backgroundColor: "#f88"}, 750, 'swing', function () {
-        //     pending.animate({backgroundColor: '#8f8'}, 750, 'swing', function () {
-        //         pending.animate({backgroundColor: "#f88"}, 750, 'swing', function () {
-        //             pending.animate({backgroundColor: '#8f8'}, 750, 'swing', function () {
-                        pending.animate({backgroundColor: "#f88"}, 750, 'swing', function () {
-
-                            var five_result_td = row.children("td:lt(4), td:last-child");
-                            var solved = row.children("td:eq(2)");
-                            var total_time = row.children("td:eq(3)");
-                            var this_time = pending.children("span").eq(0);
-                            var attm_solved = row.children("td:last-child");
-                            var attm_solved_part = attm_solved.html().split("<br>");
-                            var last_row_td = $("table:first-of-type tfoot tr:last-child").children("td").eq(pending.index());
-                            var last_row_td_part = last_row_td.html().split("<br>");
-                            var last_td_in_last_row = $("table:first-of-type tfoot tr:last-child td:last-child");
-                            var last_td_in_last_row_part = last_td_in_last_row.html().split("<br>");
-
-                            var index_text = pending.closest("tr").children("td").eq(1).text();
-                            var tmp = $('#final-table table tbody tr:has(td:contains(' + index_text + '))').children("td").eq(pending.index());
-                            var _try = parseInt(tmp.text()) ;
-                            var judged = tmp.children("span").eq(0).text();
-                            var first_accept = tmp.hasClass("first");
-                            //var tmptmp = false;
-                            //alert(index_text);
-                            //if(index_text == "qaen_university"){
-                            //    alert("ok");
-                            //    $("#container table tbody tr").each(function(){
-                            //        var ranke = $(this).children("td").eq(0);
-                            //        if(+ranke.text() == 26 && tmptmp){
-                            //            ranke.text(+ranke.text() + 1);
-                            //        }
-                            //    });
-                            //}
-
-                            pending.removeClass('pending').css("background-color", "");
-
-                            if (judged != " --") {
-
-                                pending.addClass('yes');
-                                if(first_accept){
-                                    pending.addClass("first");
-                                    // if(!(index_text == "Why Do We Fall? Sadjad University Of Technology" && pending.index() == 6/*shomare sotune soal. zero index*/)
-                                    // && !(index_text == "Why Do We Fall? Sadjad University Of Technology" && pending.index() == 4/*shomare sotune soal. zero index*/)){
-                                    //     pending.addClass("first");
-                                    // }
-                                }
-                                other_row.css('opacity', 0.2);
-                                five_result_td.addClass('sorting-row-yes');
-
-                                solved.text(parseInt(solved.text()) + 1);
-                                total_time.text(parseInt(total_time.text()) + parseInt(judged) + ((_try - 1) * 20));
-                                this_time.text(judged);
-                                tmp1 = attm_solved_part[0];
-                                tmp2 = parseInt(attm_solved_part[1]) + 1;
-                                attm_solved.html(tmp1 + "<br>" + tmp2);
-                                tmp1 = last_row_td_part[0];
-                                tmp2 = (first_accept ? this_time.text() : last_row_td_part[1]);
-                                tmp3 = parseInt(last_row_td_part[2]) + 1;
-                                last_row_td.html(tmp1 + "<br>" + tmp2 + "<br>" + tmp3);
-                                tmp1 = last_td_in_last_row_part[0];
-                                tmp2 = parseInt(last_td_in_last_row_part[1]) + 1;
-                                last_td_in_last_row.html(tmp1 + "<br>" + tmp2);
-
-                                var interval_time = (row.index() + 1 <= 5 ? 800 : (row.index() + 1 <= 10 ? 300 : (row.index() + 1 > parseInt(row.siblings("tr:last-child").children("td").eq(0).text()) / 2 ? 80 : 100)));
-                                var intervalID = setInterval(function () {
-                                    var before_soleved = parseInt(row.prev("tr").children("td").eq(2).text());
-                                    var before_time = parseInt(row.prev("tr").children("td").eq(3).text());
-                                    if (isNaN(before_soleved) || before_soleved > parseInt(solved.text()) || (before_soleved == parseInt(solved.text()) && before_time < parseInt(total_time.text()))) {
-                                        window.clearInterval(intervalID);
-                                        setTimeout(function () {
-                                            other_row.css('opacity', 1);
-                                            five_result_td.removeClass('sorting-row-yes');
-                                            set_click_handler();
-                                        }, 2000);
-                                    }
-                                    else{
-                                        row.insertBefore(row.prev());
-                                        var first_td = row.children("td:first");
-                                        var first_td_of_first_tr_sibling = row.next('tr').children("td:first");
-                                        if (parseInt(first_td.text()) > 1 && first_td.text() != first_td_of_first_tr_sibling.text()) {
-                                            first_td.text(parseInt(first_td.text()) - 1);
-                                            first_td_of_first_tr_sibling.text(parseInt(first_td_of_first_tr_sibling.text()) + 1);
-                                        }
-                                    }
-                                }, interval_time);
+        // pending.animate({backgroundColor: "#f88"}, 1000, 'linear', function () {
+        //     pending.animate({backgroundColor: '#8f8'}, 1000, 'linear', function () {
+                pending.animate({backgroundColor: "#f88"}, 1400, 'linear', function () {
+                    pending.animate({backgroundColor: '#8f8'}, 1300, 'linear', function () {
+                        pending.animate({backgroundColor: "#f88"}, 1300, 'linear', function () {
+                            console.log(parseInt(row.children("td:eq(0)").text()) <= 6);
+                            if(parseInt(row.children("td:eq(0)").text()) <= 6){
+                                pending.animate({backgroundColor: "#8f8"}, 1300, 'linear', function () {
+                                    pending.animate({backgroundColor: '#f88'}, 1300, 'linear', function () {
+                                        pending.animate({backgroundColor: "#8f8"}, 1300, 'linear', function () {
+                                            do_judje();
+                                        });
+                                    });
+                                });
                             }
                             else {
-                                pending.addClass('no');
-                                other_row.css('opacity', 0.2);
-                                five_result_td.addClass('sorting-row-no');
-
-                                //tmp1 = attm_solved_part[0];
-                                //tmp2 = attm_solved_part[1];
-                                //attm_solved.html(tmp1 + "<br>" + tmp2);
-                                //tmp1 = last_row_td_part[0];
-                                //tmp2 = last_row_td_part[1];
-                                //tmp3 = last_row_td_part[2];
-                                //last_row_td.html(tmp1 + "<br>" + tmp2 + "<br>" + tmp3);
-                                //tmp1 = last_td_in_last_row_part[0];
-                                //tmp2 = last_td_in_last_row_part[1];
-                                //last_td_in_last_row.html(tmp1 + "<br>" + tmp2);
-
-                                setTimeout(function () {
-                                    other_row.css('opacity', 1);
-                                    five_result_td.removeClass('sorting-row-no');
-                                    set_click_handler();
-                                }, 2500);
+                                do_judje();
                             }
                         });
-                    // });
-                // });
+                    });
+                });
+                function do_judje() {
+                    var five_result_td = row.children("td:lt(4), td:last-child");
+                    var solved = row.children("td:eq(2)");
+                    var total_time = row.children("td:eq(3)");
+                    var this_time = pending.children("span").eq(0);
+                    var attm_solved = row.children("td:last-child");
+                    var attm_solved_part = attm_solved.html().split("<br>");
+                    var last_row_td = $("table:first-of-type tfoot tr:last-child").children("td").eq(pending.index());
+                    var last_row_td_part = last_row_td.html().split("<br>");
+                    var last_td_in_last_row = $("table:first-of-type tfoot tr:last-child td:last-child");
+                    var last_td_in_last_row_part = last_td_in_last_row.html().split("<br>");
+
+                    var index_text = pending.closest("tr").children("td").eq(1).text();
+                    var tmp = $('#final-table table tbody tr:has(td:contains(' + index_text + '))').children("td").eq(pending.index());
+                    var _try = parseInt(tmp.text()) ;
+                    var judged = tmp.children("span").eq(0).text();
+                    var first_accept = tmp.hasClass("first");
+                    //var tmptmp = false;
+                    //alert(index_text);
+                    //if(index_text == "qaen_university"){
+                    //    alert("ok");
+                    //    $("#container table tbody tr").each(function(){
+                    //        var ranke = $(this).children("td").eq(0);
+                    //        if(+ranke.text() == 26 && tmptmp){
+                    //            ranke.text(+ranke.text() + 1);
+                    //        }
+                    //    });
+                    //}
+
+                    pending.removeClass('pending').css("background-color", "");
+
+                    if (judged != " --") {
+
+                        pending.addClass('yes');
+                        if(first_accept){
+                            pending.addClass("first");
+                            // if(!(index_text == "Why Do We Fall? Sadjad University Of Technology" && pending.index() == 6/*shomare sotune soal. zero index*/)
+                            // && !(index_text == "Why Do We Fall? Sadjad University Of Technology" && pending.index() == 4/*shomare sotune soal. zero index*/)){
+                            //     pending.addClass("first");
+                            // }
+                        }
+                        other_row.css('opacity', 0.2);
+                        five_result_td.addClass('sorting-row-yes');
+
+                        solved.text(parseInt(solved.text()) + 1);
+                        total_time.text(parseInt(total_time.text()) + parseInt(judged) + ((_try - 1) * 20));
+                        this_time.text(judged);
+                        tmp1 = attm_solved_part[0];
+                        tmp2 = parseInt(attm_solved_part[1]) + 1;
+                        attm_solved.html(tmp1 + "<br>" + tmp2);
+                        tmp1 = last_row_td_part[0];
+                        tmp2 = (first_accept ? this_time.text() : last_row_td_part[1]);
+                        tmp3 = parseInt(last_row_td_part[2]) + 1;
+                        last_row_td.html(tmp1 + "<br>" + tmp2 + "<br>" + tmp3);
+                        tmp1 = last_td_in_last_row_part[0];
+                        tmp2 = parseInt(last_td_in_last_row_part[1]) + 1;
+                        last_td_in_last_row.html(tmp1 + "<br>" + tmp2);
+
+                        var interval_time = (row.index() + 1 <= 5 ? 800 : (row.index() + 1 <= 10 ? 300 : (row.index() + 1 > parseInt(row.siblings("tr:last-child").children("td").eq(0).text()) / 2 ? 80 : 100)));
+                        var intervalID = setInterval(function () {
+                            var before_soleved = parseInt(row.prev("tr").children("td").eq(2).text());
+                            var before_time = parseInt(row.prev("tr").children("td").eq(3).text());
+                            if (isNaN(before_soleved) || before_soleved > parseInt(solved.text()) || (before_soleved == parseInt(solved.text()) && before_time < parseInt(total_time.text()))) {
+                                window.clearInterval(intervalID);
+                                setTimeout(function () {
+                                    other_row.css('opacity', 1);
+                                    five_result_td.removeClass('sorting-row-yes');
+                                    set_click_handler();
+                                }, 2000);
+                            }
+                            else{
+                                row.insertBefore(row.prev());
+                                var first_td = row.children("td:first");
+                                var first_td_of_first_tr_sibling = row.next('tr').children("td:first");
+                                if (parseInt(first_td.text()) > 1 && first_td.text() != first_td_of_first_tr_sibling.text()) {
+                                    first_td.text(parseInt(first_td.text()) - 1);
+                                    first_td_of_first_tr_sibling.text(parseInt(first_td_of_first_tr_sibling.text()) + 1);
+                                }
+                            }
+                        }, interval_time);
+                    }
+                    else {
+                        pending.addClass('no');
+                        other_row.css('opacity', 0.2);
+                        five_result_td.addClass('sorting-row-no');
+
+                        //tmp1 = attm_solved_part[0];
+                        //tmp2 = attm_solved_part[1];
+                        //attm_solved.html(tmp1 + "<br>" + tmp2);
+                        //tmp1 = last_row_td_part[0];
+                        //tmp2 = last_row_td_part[1];
+                        //tmp3 = last_row_td_part[2];
+                        //last_row_td.html(tmp1 + "<br>" + tmp2 + "<br>" + tmp3);
+                        //tmp1 = last_td_in_last_row_part[0];
+                        //tmp2 = last_td_in_last_row_part[1];
+                        //last_td_in_last_row.html(tmp1 + "<br>" + tmp2);
+
+                        setTimeout(function () {
+                            other_row.css('opacity', 1);
+                            five_result_td.removeClass('sorting-row-no');
+                            set_click_handler();
+                        }, 2500);
+                    }
+                }
             // });
         // });
     }
